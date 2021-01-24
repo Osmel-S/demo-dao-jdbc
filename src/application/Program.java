b9d59bfb499d5ad274ed248f11fd009e9f375025
Tiosams <osmelzinho@gmail.com>
2021-01-24 15:15:37 -0300

    findById implementarion

--- src/application/Program.java
+++ src/application/Program.java
@@ -2,19 +2,22 @@ package application;
 
 import java.util.Date;
 
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
 import model.entities.Department;
 import model.entities.Seller;
 
 public class Program {
 
 	public static void main(String[] args) {
+
+		// forma de injecao de dependencia sem explicitar a implementacao
+		SellerDao sellerDao = DaoFactory.createSellerDao();// dessa forma o programa nao conhece a implementacao,apenas a interface.
+	
+	
+		Seller seller = sellerDao.findById(3);
 		
-		Department obj = new Department(1,"books");
-		
-		Seller seller = new Seller(21,"bob","bob@gmail.com",new Date(),3000.00,obj);
 		System.out.println(seller);
-		
-		
 	}
 
 }
