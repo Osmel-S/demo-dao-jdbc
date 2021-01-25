9479ae4c862352655ff9c1177b7e22817fcf3794
Tiosams <osmelzinho@gmail.com>
2021-01-25 12:04:14 -0300

    update implementation

--- src/model/dao/impl/SellerDaoJDBC.java
+++ src/model/dao/impl/SellerDaoJDBC.java
@@ -41,9 +41,9 @@ public class SellerDaoJDBC implements SellerDao {
 			st.setDouble(4,obj.getBaseSalary());
 			st.setInt(5, obj.getDepartment().getId());
 			
-			int rowsAffecte = st.executeUpdate();
+			int rowsAffected = st.executeUpdate();
 			
-			if(rowsAffecte > 0) {
+			if(rowsAffected > 0) {
 				ResultSet rs = st.getGeneratedKeys();
 				if(rs.next()) {
 					int id = rs.getInt(1);
@@ -62,8 +62,27 @@ public class SellerDaoJDBC implements SellerDao {
 
 	@Override
 	public void update(Seller obj) {
-		// TODO Auto-generated method stub
-
+		PreparedStatement st = null;
+		try {
+			st = conn.prepareStatement(
+					"UPDATE seller "
+				   + "SET Name = ?,Email= ?,BirthDate= ?,BaseSalary= ?,DepartmentId=? "	
+				   + "WHERE Id= ?");
+			
+			st.setString(1, obj.getName());
+			st.setString(2, obj.getEmail());
+			st.setDate(3,new java.sql.Date(obj.getBirthdate().getTime()));
+			st.setDouble(4,obj.getBaseSalary());
+			st.setInt(5, obj.getDepartment().getId());
+			st.setInt(6, obj.getId());
+			
+			st.executeUpdate();
+			
+		}catch(SQLException e) {
+			throw new DbException(e.getMessage());
+		}finally{
+			DB.closeStatement(st);
+		}
 	}
 
 	@Override
